fadae0e409a381b21bbe7d0f2f74a943d0a99316
Hugo Perdomo <hugo@perdomo.dev>
2024-09-04 20:27:18 -0400

    added astro support and markdown autotag

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -1,11 +1,13 @@
 return {
   'windwp/nvim-ts-autotag',
   ft = {
+    'astro',
     'javascript',
     'javascriptreact',
     'typescript',
     'typescriptreact',
     'html',
+    'markdown',
   },
   config = function()
     require('nvim-ts-autotag').setup()

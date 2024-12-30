70df5ad2a5778a21275d078dc0a2c1de3319af95
Hugo Perdomo <hugo@perdomo.dev>
2024-12-30 17:36:26 -0500

    fixed new syntax with conform.nvim

--- init.lua
+++ init.lua
@@ -749,10 +749,10 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier', stop_after_first = true } },
-        typescript = { { 'prettierd', 'prettier', stop_after_first = true } },
-        typescriptreact = { { 'prettierd', 'prettier', stop_after_first = true } },
-        json = { { 'prettierd', 'prettier', stop_after_first = true } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
